1f116a25099fee0b0b15864480fbe46c2dd7c432
an288705 <aj0466012@gmail.com>
2022-03-24 00:49:37 -0400

    register API added

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,7 +1,7 @@
 const route = require('express').Router();
 const db = require('../firebase');
-const { getAuth, signInWithEmailAndPassword } = require("firebase/auth");
-const { collection, doc, getDoc } = require("firebase/firestore");
+const { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } = require("firebase/auth");
+const { collection, doc, getDoc, setDoc } = require("firebase/firestore");
 
 route.post('/',(req,result)=>{
     const auth = getAuth();
@@ -28,6 +28,52 @@ route.post('/',(req,result)=>{
         }
 
         getUserInfo();
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage)
+        return result.status(400).send({error:errorMessage});
+    });
+});
+
+route.post('/create',(req,result)=>{
+    const auth = getAuth();
+    const email = req.body.email;
+    const password = req.body.password;
+
+    console.log(req.body);
+
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        //console.log(user)
+
+        //create new firestore doc with user.uid
+        async function addNewUser(){
+            const userRef = doc(db, "users", user.uid);
+            await setDoc(userRef,{
+                addictions:[],
+                goals:[],
+                routine:[]
+            });
+
+            //now return requested into
+            let userInfo = {};
+            userInfo['_id']=user.uid;
+            userInfo['name']=user.displayName;
+            userInfo['email']=user.email;
+            userInfo['role']='free';
+            userInfo['addictions']=[];
+            userInfo['goals']=[];
+            userInfo['routine']=[];
+            console.log(userInfo);
+
+            return result.send(userInfo);
+        }
+
+        addNewUser();
     })
     .catch((error) => {
         const errorCode = error.code;
